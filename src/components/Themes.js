c749a6dccf39261a2a025c2872c80f0ecd1b51f5
manisha017 <59685443+manisha017@users.noreply.github.com>
2021-12-25 22:54:00 +0530

    first commit

---
+++ src/components/Themes.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import "../css/Themes.css";
+
+class Themes extends React.Component {
+  render() {
+      const {active} = this.props;
+      console.log("Theme",active);
+      return (
+          <div className="music">
+              <h2>Theme Select</h2>
+              <ul>
+                  {["Snow White","Black","USC Gold","Space Gray","Pearl"].map((element,index)=>{
+                      return active===index?<li key={index} className="active theme-li">{element}</li>:<li className="theme-li" key={index}>{element} </li>
+                  })}
+              </ul>
+          </div>
+
+      )
+  }
+
+}
+
+
+
+
+export default Themes;
